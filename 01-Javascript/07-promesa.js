a315272faac0564ee82a2b3756dd0c37f8ae24c1
Ronald Cargua <ronald_96c@hotmail.com>
2018-10-31 09:04:51 -0500

    deber

--- 01-Javascript/07-promesa.js
+++ 01-Javascript/07-promesa.js
@@ -88,3 +88,40 @@ promesaAppFile('08-text.txt','hola').then(
         console.log('Algo malo paso',resultadoError);
     }
 );
+
+const promesaForeach = (arregloString)=>{
+    const arregloRespuestas=[];
+    return new Promise(
+        (resolve)=>{
+            arregloString
+                .forEach(
+                    (string, indice)=>{
+                        const archivo =`${indice}-${string}.txt`;
+                        const contenido =`${string}`;
+                        fs.writeFile(archivo,
+                            contenido,
+                            (err)=>{
+                                const  respuesta = {
+                                    nombreArchivo: archivo,
+                                    contenidoArchivo: contenido,
+                                    error: err
+                                };
+                                arregloRespuestas.push(respuesta);
+                                const tamanoRespuestas = arregloRespuestas.length;
+                                if(tamanoRespuestas === arregloString.length){
+                                    resolve (arregloRespuestas);
+                                }
+                            }
+                        );
+                    }
+                );
+    });
+
+
+};
+
+const arregloString=['A','B','C'];
+console.log(promesaForeach(arregloString));
+promesaForeach(arregloString).then((contenidArreglo)=>{
+        console.log(contenidArreglo);
+    });
