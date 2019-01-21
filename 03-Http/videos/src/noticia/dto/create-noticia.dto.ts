926f2fb9b9d71ad874e583ad5fe42c6fbfa211bb
Ronald Cargua <ronald_96c@hotmail.com>
2019-01-21 11:05:24 -0500

    tipos en bootstrap

---
+++ 03-Http/videos/src/noticia/dto/create-noticia.dto.ts
@@ -0,0 +1,11 @@
+import {IsNotEmpty, IsOptional, IsString} from "class-validator";
+
+export class CreateNoticiaDto {
+
+    @IsNotEmpty()
+    @IsString()
+    titulo:string;
+    @IsOptional()
+    @IsString()
+    descripcion: string;
+}
